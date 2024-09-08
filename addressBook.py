32bd1b9973a73eab0564dc42ecfc96798006ee89
Nick Chernock <n.chernock@gmail.com>
2024-09-08 01:18:17 -0400

    search_book construction
    - search_book built but isn't accurate
        - number / email not being found
    - expanded default book
    - changed naming convention to appropriate python guidelines

--- addressBook.py
+++ addressBook.py
@@ -7,18 +7,55 @@ class AddressBook:
             "entry1" : { 
                 "First Name:" :"John",
                 "Last Name:" : "Smith",
-                "Phone Number:" : 1234567890,
+                "Phone Number:" : "1234567890",
                 "emailAddress:" : "jSmith@aol.com"
             },
             "entry2" : {
                 "First Name:" : "Jane",
                 "Last Name:" : "Smith",
-                "Phone Number:" : 9870546842,
+                "Phone Number:" : "9870546842",
                 "Email Address:" : "JaneSmith@aol.com"
             },
+            "entry3" : {
+                "First Name:" : "Timothy",
+                "Last Name:" : "Nickles",
+                "Phone Number:" : "4959483847",
+                "Email Address:" : "nickleTim@gmail.com"
+            },
+            "entry4": {
+                "First Name:": "Brad",
+                "Last Name:": "Louis",
+                "Phone Number:": "82319405583",
+                "Email Address:": "blouy@hotmail.com"
+            },
+            "entry5": {
+                "First Name:": "Peter",
+                "Last Name:": "Love",
+                "Phone Number:": "2948590123",
+                "Email Address:": "plove@gmail.com"
+            },
+            "entry6": {
+                "First Name:": "Hubert",
+                "Last Name:": "Frank",
+                "Phone Number:": "1039458394",
+                "Email Address:": "hubertser@aol.com"
+            },
+            "entry7": {
+                "First Name:": "Keith",
+                "Last Name:": "Teradello",
+                "Phone Number:": "9234859348",
+                "Email Address:": "tdello@yahoo.com"
+            },
+            "entry8": {
+                "First Name:": "Geoff",
+                "Last Name:": "Clark",
+                "Phone Number:": "9380249287",
+                "Email Address:": "gclark@gmail.com"
+            },
+
         }
 
-    def printBook(self, dict):
+    def print_book(self, dict):
         print("\n\n---------------------------")
         print("-------" + self.name + "--------")
         print("---------------------------")
@@ -27,42 +64,48 @@ class AddressBook:
                 print(key, value)
             print("---------------------------")
 
-    def addEntry(self, newEntry):
-        bookCount = self.countBook()
-        userEntry = {("Entry" + str(bookCount + 1)): newEntry}
-        self.book.update(userEntry)
+    def add_entry(self, new_entry):
+        book_count = self.count_book()
+        user_entry = {("Entry" + str(book_count + 1)): new_entry}
+        self.book.update(user_entry)
 
-    def countBook(self):
-        bookList = len(self.book)
-        return bookList
+    def count_book(self):
+        book_list = len(self.book)
+        return book_list
     
-    def clearBook(self):
+    def clear_book(self):
         for i in self.book.copy():
             del self.book[i]
 
-    def searchBook(self, searchType, searchValue):
-        # open dictionary to nested dictionary
-        # match search type with nested dict key
-        #   for each key in each nested dict
-        #       if searchValue == nested dict value
-        #           display found entry
-        searchValue = searchValue.lower()
+    def search_book(self, search_type, search_value):
+        print("---------------------------")
+        print("DEBUG: Searching Book...")
+        print("---------------------------")
+        search_value = str(search_value).lower()
+        found_entries = []
+        # iterate over each entry in book
         for entry in self.book:
-            for key, value in self.book[entry].items:
-                lowerCaseKey = key.lower()
-                if searchType in lowerCaseKey:
-                    if searchValue in value:
-                        return self.book[entry]
-                # if searchType == "first name":
-                #     pass
-                # elif searchType == "last name":
-                #     pass
-                # elif searchType == "phone number":
-                #     pass
-                # elif searchType == "email address":
-                #     pass
-    
-    def debugPrintRawBook(self):
+            if self.book[entry] in found_entries:
+                break
+            # iterate over each entry items in book
+            for key, value in self.book[entry].items():
+                key = key.lower()
+                value = value.lower()
+                # if search_type doesnt match key, move to next iteration
+                if search_type not in key:
+                    break
+                # if search_value matches value, add entry to found_entries
+                if search_value in value:
+                    found_entries.append(self.book[entry])
+                    break
+        # if found_entries has atleast 1 entry, print entry
+        if len(found_entries) >= 1:
+            for entry in found_entries:
+                for key, value in entry.items():
+                    print(key, value)
+                print("---------------------------")
+                
+    def debug_print_raw_book(self):
         print(self.book.keys())
         for i in self.book:
             print(self.book[i])
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from addressBook import AddressBook
 
 book = AddressBook()
 
-def mainMenu():
+def main_menu():
     print("----------------------------------------------")
     print("Welcome to the Address Book")
     print("----------------------------------------------")
@@ -25,9 +25,9 @@ def mainMenu():
     print("5. Clear Address Book")
     print("6. Quit")
     print("----------------------------------------------")
-    selectMainMenu()
+    select_main_menu()
     
-def selectMainMenu():
+def select_main_menu():
     selection = input("Enter a number to continue... ")
 
     
@@ -38,84 +38,81 @@ def selectMainMenu():
     
     if selection == "1": # 1. Add an Entry [WORKS]
         print("DEBUG: Selection = 1")
-        addEntryToBook() 
+        add_entry_to_book()
     elif selection == "2": # 2. Remove an Entry
         print("DEBUG: Selection = 2")
     elif selection == "3": # 3. Search for an Entry
         print("DEBUG: Selection = 3")
-        searchAddressBook()
+        search_address_book()
     elif selection == "4": # 4. Print the Address Book [WORKS + DEBUG CHECK]
         print("DEBUG: Selection = 4")
-        printAddressBook() 
+        print_address_book()
     elif selection == "5": # 5. Clear Address Book [WORKS - book needs clarification when empty]
         print("DEBUG: Selection = 5")
-        clearAddressBook()
+        clear_address_book()
     elif selection == "6": # 6. Quit [WORKS]
         print("DEBUG: Selection = 6")
         exit(1)
     else:
         print("Selection not found...")
-        selectMainMenu()
+        select_main_menu()
 
-def addEntryToBook():
+def add_entry_to_book():
     print("\n\nEnter information to add entry to Address Book")
-    firstName = input("First Name: ")
-    lastName = input("Last Name: ")
-    phoneNumber = input("Phone Number: ")
-    emailAddress = input("Email Address: ")
-    newEntry = {
-        "First Name: " : firstName,
-        "Last Name: " : lastName,
-        "Phone Number: " : phoneNumber,
-        "Email Address: " : emailAddress
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    phone_number = input("Phone Number: ")
+    email_address = input("Email Address: ")
+    new_entry = {
+        "First Name: " : first_name,
+        "Last Name: " : last_name,
+        "Phone Number: " : phone_number,
+        "Email Address: " : email_address
     }
-    book.addEntry(newEntry)
+    book.add_entry(new_entry)
     print("Entry Added!")
-    returnToMainMenu()
+    return_to_main_menu()
 
-def printAddressBook():
-    book.printBook(book.book)
-    print(book.debugPrintRawBook())
-    returnToMainMenu()
+def print_address_book():
+    book.print_book(book.book)
+    print(book.debug_print_raw_book())
+    return_to_main_menu()
 
-def searchAddressBook():
+def search_address_book():
     print("\nSelect your search type....")
-    print("1. First Name")
-    print("2. Last Name")
-    print("3. Phone Number")
-    print("4. Email Address")
+    print("1. Name")
+    print("2. Phone Number")
+    print("3. Email Address")
     selection = input("Input your selection: ")
-    if selection == 1:
-        searchType = "first name"
-        searchValue = input("\nEnter first name to search by: ")
-        print(book.searchBook(searchType, searchValue))
-    elif selection == 2:
-        searchType = "last name"
-        searchValue = input("\nEnter last name to search by: ")
-        print(book.searchBook(searchType, searchValue))
-    elif selection == 3:
-        searchType = "phone number"
-        searchValue = input("\nEnter phone number to search by: ")
-        print(book.searchBook(searchType, searchValue))
-    elif selection == 4:
-        searchType = "email address"
-        searchValue = input("\nEnter email address to search by: ")
-        print(book.searchBook(searchType, searchValue))
+    if selection == "1":
+        search_type = "name"
+        search_value = input("\nEnter first name to search by: ")
+        book.search_book(search_type, search_value)
+    elif selection == "2":
+        search_type = "number"
+        search_value = input("\nEnter phone number to search by: ")
+        book.search_book(search_type, search_value)
+    elif selection == "3":
+        search_type = "email"
+        search_value = input("\nEnter email address to search by: ")
+        book.search_book(search_type, search_value)
+    print("Address Book searched...")
+    return_to_main_menu()
 
-def clearAddressBook():
-    book.clearBook()
+def clear_address_book():
+    book.clear_book()
     print("Address Book is cleared!")
-    returnToMainMenu()
+    return_to_main_menu()
 
-def returnToMainMenu():
-    enterInput = input("Press enter to continue to the Main Menu...")
-    if enterInput == "":
-        mainMenu()
+def return_to_main_menu():
+    enter_input = input("Press enter to continue to the Main Menu...")
+    if enter_input == "":
+        main_menu()
     else:
-        returnToMainMenu()  
+        return_to_main_menu()
 
 
 
-mainMenu()
+main_menu()
 
 
