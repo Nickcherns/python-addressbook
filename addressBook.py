aa864288a0be14c4397e2e81a089628bb0ce609e
Nick Chernock <n.chernock@gmail.com>
2024-09-08 18:53:51 -0400

    Complete structure finished
    - added remove entry functionality
    - completed search book functionality
    - added true test entry
    - Completed functionality for all intended uses regardless of validation
    - cleaned up some console depictions
    - commented out all DEBUG code

--- addressBook.py
+++ addressBook.py
@@ -13,7 +13,7 @@ class AddressBook:
             "entry2" : {
                 "First Name:" : "Jane",
                 "Last Name:" : "Smith",
-                "Phone Number:" : "9870546842",
+                "Phone Number:" : "9870536842",
                 "Email Address:" : "JaneSmith@aol.com"
             },
             "entry3" : {
@@ -25,13 +25,13 @@ class AddressBook:
             "entry4": {
                 "First Name:": "Brad",
                 "Last Name:": "Louis",
-                "Phone Number:": "82319405583",
+                "Phone Number:": "8231905583",
                 "Email Address:": "blouy@hotmail.com"
             },
             "entry5": {
                 "First Name:": "Peter",
                 "Last Name:": "Love",
-                "Phone Number:": "2948590123",
+                "Phone Number:": "2978590123",
                 "Email Address:": "plove@gmail.com"
             },
             "entry6": {
@@ -52,13 +52,21 @@ class AddressBook:
                 "Phone Number:": "9380249287",
                 "Email Address:": "gclark@gmail.com"
             },
+            "entry9": {
+                "First Name:": "Test",
+                "Last Name:": "Test",
+                "Phone Number:": "0000000000",
+                "Email Address:": "test@test.com"
+            },
 
         }
 
     def print_book(self, dict):
-        print("\n\n---------------------------")
-        print("-------" + self.name + "--------")
+        print("\n---------------------------")
+        print("------ " + self.name + " -------")
         print("---------------------------")
+        if len(self.book) == 0:
+            print("  ** NO ENTRIES FOUND **")
         for entry in dict:
             for key, value in dict[entry].items():
                 print(key, value)
@@ -77,9 +85,37 @@ class AddressBook:
         for i in self.book.copy():
             del self.book[i]
 
+    def remove_entry(self, entry_name):
+        del self.book[entry_name]
+        print("Entry Removed...")
+
+    def return_entry_by_email(self, email_address):
+        email_address = email_address.lower()
+        found_entry = []
+        entry_name = ""
+        for entry in self.book:
+            for key, value in self.book[entry].items():
+                key = key.lower()
+                value = value.lower()
+                if "email" not in key:
+                    continue
+                if email_address in value:
+                    found_entry.append(self.book[entry])
+                    entry_name = entry
+        if len(found_entry) > 1:
+            return "Too many entries found..."
+        else:
+            print("---------------------------")
+            print("------- Entry Found -------")
+            print("---------------------------")
+            for key, value in found_entry[0].items():
+                print(key, value)
+            print("---------------------------")
+            return entry_name
+
     def search_book(self, search_type, search_value):
         print("---------------------------")
-        print("DEBUG: Searching Book...")
+        # print("DEBUG: Searching Book...")
         print("---------------------------")
         search_value = str(search_value).lower()
         found_entries = []
@@ -91,14 +127,14 @@ class AddressBook:
             for key, value in self.book[entry].items():
                 key = key.lower()
                 value = value.lower()
-                # if search_type doesnt match key, move to next iteration
+                # if search_type doesn't match key, move to next iteration
                 if search_type not in key:
-                    break
+                    continue
                 # if search_value matches value, add entry to found_entries
                 if search_value in value:
                     found_entries.append(self.book[entry])
                     break
-        # if found_entries has atleast 1 entry, print entry
+        # if found_entries has at least 1 entry, print entry
         if len(found_entries) >= 1:
             for entry in found_entries:
                 for key, value in entry.items():
@@ -107,7 +143,7 @@ class AddressBook:
                 
     def debug_print_raw_book(self):
         print(self.book.keys())
-        for i in self.book:
-            print(self.book[i])
+        # for i in self.book:
+        #     print(self.book[i])
 
 
--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 #   a. email address
 #   b. phone number
 #   c. names (numbers not allowed)
-# 2. prettify
+# 2. clean up program
 # 3. when address book is empty, have it display an empty message
+# 4. make sure remove_entry only takes email address
 #
 #
 
@@ -15,54 +16,54 @@ from addressBook import AddressBook
 book = AddressBook()
 
 def main_menu():
-    print("----------------------------------------------")
+    print("---------------------------")
     print("Welcome to the Address Book")
-    print("----------------------------------------------")
+    print("---------------------------")
     print("1. Add an Entry")
     print("2. Remove an Entry")
     print("3. Search for an Entry")
     print("4. Print the Address Book")
     print("5. Clear Address Book")
     print("6. Quit")
-    print("----------------------------------------------")
+    print("---------------------------")
     select_main_menu()
     
 def select_main_menu():
-    selection = input("Enter a number to continue... ")
-
-    
-    
-    
-    
-    
-    
+    selection = input("Enter a number to continue: ")
+    print("---------------------------")
     if selection == "1": # 1. Add an Entry [WORKS]
-        print("DEBUG: Selection = 1")
+        # print("DEBUG: Selection = 1")
         add_entry_to_book()
-    elif selection == "2": # 2. Remove an Entry
-        print("DEBUG: Selection = 2")
-    elif selection == "3": # 3. Search for an Entry
-        print("DEBUG: Selection = 3")
+    elif selection == "2": # 2. Remove an Entry [WORKS]
+        # print("DEBUG: Selection = 2")
+        remove_entry()
+    elif selection == "3": # 3. Search for an Entry [WORKS]
+        # print("DEBUG: Selection = 3")
         search_address_book()
     elif selection == "4": # 4. Print the Address Book [WORKS + DEBUG CHECK]
-        print("DEBUG: Selection = 4")
+        # print("DEBUG: Selection = 4")
         print_address_book()
     elif selection == "5": # 5. Clear Address Book [WORKS - book needs clarification when empty]
-        print("DEBUG: Selection = 5")
+        # print("DEBUG: Selection = 5")
         clear_address_book()
     elif selection == "6": # 6. Quit [WORKS]
-        print("DEBUG: Selection = 6")
+        # print("DEBUG: Selection = 6")
         exit(1)
     else:
         print("Selection not found...")
         select_main_menu()
 
 def add_entry_to_book():
-    print("\n\nEnter information to add entry to Address Book")
+    print("Enter information to add entry to Address Book")
+    print("---------------------------")
     first_name = input("First Name: ")
+    print("---------------------------")
     last_name = input("Last Name: ")
+    print("---------------------------")
     phone_number = input("Phone Number: ")
+    print("---------------------------")
     email_address = input("Email Address: ")
+    print("---------------------------")
     new_entry = {
         "First Name: " : first_name,
         "Last Name: " : last_name,
@@ -70,33 +71,51 @@ def add_entry_to_book():
         "Email Address: " : email_address
     }
     book.add_entry(new_entry)
-    print("Entry Added!")
+    print("    ENTRY ADDED!")
+    print("---------------------------")
     return_to_main_menu()
 
 def print_address_book():
     book.print_book(book.book)
-    print(book.debug_print_raw_book())
+    # print(book.debug_print_raw_book())
     return_to_main_menu()
 
+def remove_entry():
+    search_email = input("Enter an entry's email address you would like to remove: ")
+    search_results = book.return_entry_by_email(search_email)
+    if "Too many" in search_results:
+        print(search_results)
+        print("Try Again...")
+        remove_entry()
+    else:
+        book.remove_entry(search_results)
+        return_to_main_menu()
+
 def search_address_book():
-    print("\nSelect your search type....")
+    print("  Select your search type")
+    print("---------------------------")
     print("1. Name")
     print("2. Phone Number")
     print("3. Email Address")
+    print("---------------------------")
     selection = input("Input your selection: ")
     if selection == "1":
         search_type = "name"
-        search_value = input("\nEnter first name to search by: ")
+        print("---------------------------")
+        search_value = input("Enter first name to search by: ")
         book.search_book(search_type, search_value)
     elif selection == "2":
         search_type = "number"
-        search_value = input("\nEnter phone number to search by: ")
+        print("---------------------------")
+        search_value = input("Enter phone number to search by: ")
         book.search_book(search_type, search_value)
     elif selection == "3":
         search_type = "email"
-        search_value = input("\nEnter email address to search by: ")
+        print("---------------------------")
+        search_value = input("Enter email address to search by: ")
         book.search_book(search_type, search_value)
     print("Address Book searched...")
+    print("---------------------------")
     return_to_main_menu()
 
 def clear_address_book():
@@ -106,6 +125,7 @@ def clear_address_book():
 
 def return_to_main_menu():
     enter_input = input("Press enter to continue to the Main Menu...")
+    print("---------------------------")
     if enter_input == "":
         main_menu()
     else:
